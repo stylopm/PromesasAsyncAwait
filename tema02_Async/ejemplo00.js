8df36a00dbaae6c7406e995f30f3d34842e40aa4
stylopm <apmurillo99@hotmail.com>
2022-06-05 01:35:54 -0500

    feat: Update files

---
+++ tema02_Async/ejemplo00.js
@@ -0,0 +1,8 @@
+ let  mostrar = async ( ) => {
+    let myPromise = new Promise(function(resolve, reject) {
+      resolve("Animos Ucampers");
+    });
+    console.log(await myPromise) ;
+  }
+  
+  mostrar();
